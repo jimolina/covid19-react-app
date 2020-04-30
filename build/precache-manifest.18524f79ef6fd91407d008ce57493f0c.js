1b2d9782c1e428d31e2562d33c8c2b93c211e725
Jose Molina <israel.molina@gmail.com>
2020-04-30 18:11:07 -0400

    Initital Build folder

---
+++ build/precache-manifest.18524f79ef6fd91407d008ce57493f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24dfb7048e16539d4adbce5a64690e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a14c64e4eacd437439",
+    "url": "/static/css/main.43844555.chunk.css"
+  },
+  {
+    "revision": "15420459ec369f56cf4e",
+    "url": "/static/js/2.97fbddb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97fbddb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a14c64e4eacd437439",
+    "url": "/static/js/main.0c440566.chunk.js"
+  },
+  {
+    "revision": "39b9c28c1756ef7dde8d",
+    "url": "/static/js/runtime-main.c6904ac9.js"
+  }
+]);
